9ea47d228243554a3859edf322a79894444cdfc3
Mike <mikegarrison@alum.mit.edu>
2022-11-04 10:56:35 -0700

    Output changes

--- tournament.py
+++ tournament.py
@@ -170,15 +170,104 @@ def run_tournament(number_of_runs) -> dict:
             else:
                 old_total = ff_school.get("FF",0)
                 results[school]["FF"] = old_total + 1
+        for school in losers["round 4"]:
+            elite_school = results.get(school)
+            if not elite_school:
+                results[school] = {"E8": 1}
+            else:
+                old_total = elite_school.get("E8",0)
+                results[school]["E8"] = old_total + 1
+        for school in losers["round 3"]:
+            sweet_school = results.get(school)
+            if not sweet_school:
+                results[school] = {"S6": 1}
+            else:
+                old_total = sweet_school.get("S6",0)
+                results[school]["S6"] = old_total + 1
+        for school in losers["round 2"]:
+            sub_school = results.get(school)
+            if not sub_school:
+                results[school] = {"R2": 1}
+            else:
+                old_total = sub_school.get("R2",0)
+                results[school]["R2"] = old_total + 1
+        for school in losers["round 1"]:
+            done_school = results.get(school)
+            if not done_school:
+                results[school] = {"zilch": 1}
+            else:
+                old_total = done_school.get("zilch",0)
+                results[school]["zilch"] = old_total + 1
 
 
     return results
 
 # run
-NUMBER_OF_RUNS = 10000
+NUMBER_OF_RUNS = 1000
 bracket_check()
 final_results = run_tournament(NUMBER_OF_RUNS)
-print(final_results)
-
-
+for school in final_results:
+    print(str(school)+" Champs: "+str(final_results[school].get("champion",0))+" #2: "+str(final_results[school].get("runner up",0))+" FF: "+str(final_results[school].get("FF",0)))
+    print(" -- Elite 8: "+str(final_results[school].get("E8",0))+" Sweet 16: "+str(final_results[school].get("S6",0))+" won a match: "+str(final_results[school].get("R2",0)))
+    print(" -- one and done: "+str(final_results[school].get("zilch",0)))
+# list of champions
+list_of_champs = []
+for school in final_results:
+    championships = float(final_results[school].get("champion",0))
+    if championships >= float(NUMBER_OF_RUNS)*0.05:
+        name = str(school)
+        champ_pct = championships / float(NUMBER_OF_RUNS) * 100
+        list_of_champs.append([name,champ_pct])
+sorted_list_of_champs = sorted(list_of_champs,key=lambda x: x[1],reverse=True)
+print("Championship Percentages")
+for champs in sorted_list_of_champs:
+    print(str(champs[0])+": "+str(round(champs[1],1)))
+# list of teams reaching semis
+list_of_semis = []
+for school in final_results:
+    championships = float(final_results[school].get("champion",0))
+    runner = float(final_results[school].get("runner up",0))
+    semis = championships + runner
+    if semis >= float(NUMBER_OF_RUNS)*0.10 or championships >= float(NUMBER_OF_RUNS)*0.05:
+        name = str(school)
+        semi_pct = semis / float(NUMBER_OF_RUNS) * 100
+        list_of_semis.append([name,semi_pct])
+sorted_list_of_semis = sorted(list_of_semis,key=lambda x: x[1],reverse=True)
+print("Semi-finalist Percentages")
+for semis in sorted_list_of_semis:
+    print(str(semis[0])+": "+str(round(semis[1],1)))
+# list of teams reaching FF
+list_of_FF = []
+for school in final_results:
+    championships = float(final_results[school].get("champion",0))
+    runner = float(final_results[school].get("runner up",0))
+    final4 = float(final_results[school].get("FF",0))
+    semis = championships + runner
+    f4 = championships + runner + final4
+    if semis >= float(NUMBER_OF_RUNS)*0.10 or championships >= float(NUMBER_OF_RUNS)*0.05 or f4 >= float(NUMBER_OF_RUNS)*0.15:
+        name = str(school)
+        f4_pct = f4 / float(NUMBER_OF_RUNS) * 100
+        list_of_FF.append([name,f4_pct])
+sorted_list_of_FF = sorted(list_of_FF,key=lambda x: x[1],reverse=True)
+print("Final Four Percentages")
+for teams in sorted_list_of_FF:
+    print(str(teams[0])+": "+str(round(teams[1],1)))
+# list of teams reaching E8
+list_of_E8 = []
+for school in final_results:
+    championships = float(final_results[school].get("champion",0))
+    runner = float(final_results[school].get("runner up",0))
+    final4 = float(final_results[school].get("FF",0))
+    elite8 = float(final_results[school].get("E8",0))
+    semis = championships + runner
+    f4 = championships + runner + final4
+    e8 = championships + runner + final4 + elite8
+    if semis >= float(NUMBER_OF_RUNS)*0.10 or championships >= float(NUMBER_OF_RUNS)*0.05 or f4 >= float(NUMBER_OF_RUNS)*0.15 or e8 >= float(NUMBER_OF_RUNS)*0.20:
+        name = str(school)
+        e8_pct = e8 / float(NUMBER_OF_RUNS) * 100
+        list_of_E8.append([name,e8_pct])
+sorted_list_of_E8 = sorted(list_of_E8,key=lambda x: x[1],reverse=True)
+print("Elite Eight Percentages")
+for teams in sorted_list_of_E8:
+    print(str(teams[0])+": "+str(round(teams[1],1)))
     
